7b3b58f7c8da71de77d17ac96663ccd21e2730e4
Alex Spock <awspock@gmail.com>
2025-12-25 10:14:30 -0700

    updated store summary to show receipt dates as dates and not include time

--- pub/pages/store/summary.php
+++ pub/pages/store/summary.php
@@ -25,7 +25,7 @@
         <div data-id="edit"><a href="/account/ACCOUNT_ID/receipt/RECEIPT_ID/edit" target="_blank"><i class="fa-solid fa-pencil"></i></a></div>
         <h3 data-id="account"></h3>
         <div class="receipt-info">
-            <div data-id="date" data-dateformatter></div>
+            <div data-id="date" data-dateonlyformatter></div>
             <div data-id="amount" data-moneyformatter></div>
             <div data-id="size" data-bitesformatter></div>
             <div data-id="type"></div>
